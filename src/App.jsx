1165c0c54148c2533816afba9993f679dc3b18d6
Rahul <rahulsaha6917@gmail.com>
2026-04-13 00:07:21 +0530

    Refactor App component and update imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,48 @@
-import { useState, useEffect } from 'react';
-import Taskform from './Components/Taskform'
-import Tasklist from './Components/Tasklist';
-import Progresstracker from './Components/Progresstracker'
-import './Sd.css'
-
+import Taskform from "./Components/Taskform";
+import TaskList from "./Components/TaskList";
+import Progresstracker from "./Components/Progresstracker";
+import { useEffect, useState } from "react";
+import "./Style.css";
 
 export default function App() {
   const [tasks, setTasks] = useState([]);
 
-
-  useEffect(()=>{
-    localStorage.setItem("tasks",JSON.stringify(tasks));
+  useEffect(() => {
+    localStorage.setItem
+    ("tasks", JSON.stringify(tasks))
   });
 
-
-  const addTask = (task)=>{
-    setTasks([...tasks, task])
+  const addTask = (task) => {
+    setTasks([...tasks,task]);
   }
-
 
   const updateTask = (updatedTask, index) => {
     const newtask = [...tasks];
-    newtask[index] = updatedTask;
+     newtask[index] = updatedTask;
     setTasks(newtask);
   }
   const deleteTask = (index) => {
-    setTasks(tasks.filter((_, i) => i != index ))
+      setTasks(tasks.filter((_, i) => i != index));
   }
-
 
   const clearTasks = () => {
     setTasks([]);
   }
-  return (
-    <div>
-        
+
+  return(
+    <div className="App">
       <header>
-        <h1>GymMan</h1>
-        <p><i>Welcome Your friendly Gym Manager</i></p>
+        <h1 className="title">Your Task</h1>
+        <p className="tagline">Our friendly TaskManager</p>
       </header>
 
-    
       <Taskform addTask = {addTask}/>
-      <Tasklist tasks = {tasks}
-       updateTask = {updateTask} 
-       deleteTask = {deleteTask}/>
+      <TaskList tasks = {tasks} 
+      updateTask = {updateTask}
+      deleteTask = {deleteTask}/>
       <Progresstracker tasks = {tasks}/>
-
-
-      {tasks.length>0 && (<button className='clear-btn'
-      onClick={clearTasks}>clear All Tasks</button>)}
+      {tasks.length>0 && 
+      (<button onClick={clearTasks} className="clear-btn">Clear all tasks</button>)}
       
     </div>
   )
