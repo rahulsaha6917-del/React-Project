8812afbebde80572ca51f3bb8d9a026efc2e8966
Rahul <rahulsaha6917@gmail.com>
2026-04-13 00:14:48 +0530

    Refactor TaskList component structure and formatting

--- src/Components/Tasklist.jsx
+++ src/Components/Tasklist.jsx
@@ -1,28 +1,26 @@
-import React from 'react'
+export default function TaskList({tasks , updateTask, deleteTask}) {
 
-
-export default function TaskList({tasks, updateTask, deleteTask}) {
-    const toggleComplete = (index)=>{
+  const toggleComplete = (index)=>{
         const updatedTask = {...tasks[index], completed: !tasks[index].completed};
         updateTask(updatedTask, index);
     }
   return (
-    <ul className='task-list'>
-        {tasks.map((task, index)=>(
-            <li key={index} className= {task.completed ? 'completed': ''}>
-                <div>
-                    <span>{task.text}</span>
-                    <small>({task.priority}, {task.category})</small>
-                </div>
+    <ul className="task-list">
+      {tasks.map((task, index)=> (
+        <li key={index} className={task.completed ? "completed" : ""}>
+          <div>
+            <span>{task.text}
+              <small>({task.priority} , {task.category})</small>
+            </span>
+          </div>
 
-
-                <div>
-                    <button onClick={()=>toggleComplete(index)}
-                    >{task.completed ? "Undo" : "Complete"}</button>
-                    <button onClick={()=>deleteTask(index)}>Delete</button>
-                </div>
-            </li>
-        ))}
+          <div>
+            <button onClick={() => toggleComplete(index)}>
+              {task.completed ? "Undo" : "Complete"}</button>
+            <button onClick={() => deleteTask(index)}>Delete</button>
+          </div>
+        </li>
+      ))}
     </ul>
   )
 }
